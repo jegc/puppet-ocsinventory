eb562e37254dce8d1ba7aeaf1856b34d68931300
Jorge Ernesto Guevara Cuenca <jorgeg@loyal3.com>
2016-07-01 13:11:10 -0500

    Add ubuntu support

--- manifests/params.pp
+++ manifests/params.pp
@@ -4,11 +4,11 @@ class ocsinventory::params {
   $agent             = true
   $agent_ocs_mode    = 'cron'
   $agent_ocs_pause   = 100
-  $agent_ocs_server  = undef
-  $agent_ocs_tag     = undef
+  $agent_ocs_server  = localhost
+  $agent_ocs_tag     = tag
   $pkg_agent_ensure  = present
   $pkg_server_ensure = present
-  $server            = false
+  $server            = true
 
   case $::osfamily {
     'RedHat': {
@@ -45,6 +45,27 @@ class ocsinventory::params {
 
       $pkg_list_server = [
         'ocsinventory',
+        'ocsinventory-reports',
+        'ocsinventory-server',
+      ]
+    }
+
+    'Debian': {
+      $config_dir         = '/etc/ocsinventory'
+      $config_dir_mode    = '0755'
+      $config_dir_recurse = false
+      $config_file_mode   = '0644'
+      $config_group       = 'root'
+      $config_user        = 'root'
+      $log_dir_agent      = '/var/log/ocsinventory-agent'
+      $log_dir_server     = '/var/log/ocsinventory-server'
+      $pkg_deps           = undef
+
+      $pkg_list_agent = [
+        'ocsinventory-agent',
+      ]
+
+      $pkg_list_server = [
         'ocsinventory-reports',
         'ocsinventory-server',
       ]
